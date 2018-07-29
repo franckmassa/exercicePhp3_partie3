8d2f24868b55958cafbfbd5d33590c84d6c1c709
massa <massafranck@gmail.com>
2018-07-29 14:08:40 +0200

    initial

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$number1 = 100;
+$number2 = 1;
+ ?>
+ <!DOCTYPE html>
+ <html lang="fr">
+   <head>
+     <meta charset="utf-8" />
+     <title>Exercice3</title>
+   </head>
+   <body>
+     <p>
+<?php
+//Décrémentation de la  premère variable tant qu'elle n'est pas inférieure ou égale à 10 
+while($number1 >= 10){
+  echo $number1 * $number2 . '<br/>';
+  $number1--;
+}
+ ?>
+     </p>
+   </body>
+ </html>
